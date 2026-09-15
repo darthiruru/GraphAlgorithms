5c32da497f9d13f54560e4f833d8b011d2c05144
darthiruru <i.gribkina@gmail.com>
2026-09-15 05:08:53 +0300

    Change folder for task A

--- task_A.py
+++
@@ -1,8 +0,0 @@
-def count_roads(roads, n):
-    ans = sum([sum(row) for row in roads]) / 2
-    return int(ans)
-
-if __name__ == '__main__':
-    n = int(input())
-    roads = [list(map(int, input().split())) for _ in range(n)]
-    print(count_roads(roads, n))
